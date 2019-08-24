14a34ee94ab79b6cd0075a73217acb04c8235c00
root <rtlepple@gmail.com>
2019-08-24 21:03:15 +0000

    initial commit

---
+++ bin/setup.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+#########################################################
+# Input parameters
+#########################################################
+
+case "$1" in
+        aws)
+           echo "you chose aws"
+            ;;
+        azure)
+           echo "you chose azure"
+            ;;
+        gcp)
+	   echo "you chose gcp"
+            ;;
+        *)
+            echo $"Usage: $0 {aws|azure|gcp} template-file [docker-device]"
+            echo $"example: ./setup.sh azure default_template.json"
+            echo $"example: ./setup.sh aws cdsw_template.json /dev/xvdb"
+            exit 0
+esac
+
+CLOUD_PROVIDER=$1
