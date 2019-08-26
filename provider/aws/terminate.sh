838d4feb0e63b73098c5cf349eecd1fe05df590a
root <rtlepple@gmail.com>
2019-08-26 23:24:35 +0000

    initial commit

---
+++ provider/aws/terminate.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+###########################################################################################################
+# import parameters and utility functions 
+###########################################################################################################
+. $starting_dir/provider/aws/demo.properties
+. $starting_dir/provider/aws/prereq_utils.sh
+. $starting_dir/provider/aws/.info
+
+
+# function for logging
+log() {
+    echo "[$(date)]: $*"
+    echo "[$(date)]: $*" >> terminate.log
+}
+
+# delete all the prereqs by calling the function
+terminate_prereqs
+
