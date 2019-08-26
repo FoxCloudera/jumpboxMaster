e252fd048bc971cc7016674869f98181a865b251
root <rtlepple@gmail.com>
2019-08-26 15:03:40 +0000

    initial commit

--- provider/aws/aws_setup.sh
+++ provider/aws/aws_setup.sh
@@ -9,20 +9,31 @@
 #####################################################
 
 
-log "Installing AWS_CLI"
-aws_cli_version=`aws --version 2>&1`
-log "Current CLI version: $aws_cli_version"
-if [[ $aws_cli_version = *"aws-cli"* ]]; then
-    log "AWS CLI already installed. Skipping"
-    return
-fi
+#log "Installing AWS_CLI"
+#aws_cli_version=`aws --version 2>&1`
+#log "Current CLI version: $aws_cli_version"
+#if [[ $aws_cli_version = *"aws-cli"* ]]; then
+#    log "AWS CLI already installed. Skipping"
+#    return
+#fi
 
-if [ $machine = 'Linux' ]; then
-	yum -y install unzip
-fi 
+#if [ $machine = 'Linux' ]; then
+#	yum -y install unzip
+#fi 
 
-curl -s -O "https://s3.amazonaws.com/aws-cli/awscli-bundle.zip"
-unzip awscli-bundle.zip
-./awscli-bundle/install -i /usr/local/aws -b /usr/local/bin/aws
-rm -rf awscli-bundle*
-log "Done installing AWS CLI"
+#curl -s -O "https://s3.amazonaws.com/aws-cli/awscli-bundle.zip"
+#unzip awscli-bundle.zip
+#./awscli-bundle/install -i /usr/local/aws -b /usr/local/bin/aws
+#rm -rf awscli-bundle*
+#log "Done installing AWS CLI"
+
+###########################################################################################################
+# import parameters and auxiliary functions 
+###########################################################################################################
+. /app/jumpboxMaster/provider/aws/prereq_utils.sh
+
+
+#####################################################
+#	Step 1: install the AWS cli
+#####################################################
+install_aws_cli
--- provider/aws/prereq_utils.sh
+++ provider/aws/prereq_utils.sh
@@ -5,5 +5,51 @@
 ###########################################################################################################
 
 #####################################################
-# Function: create AWS prereqs (VPC, subnets, security group, db subnet)
+# Functions: create AWS prereqs (VPC, subnets, security group, db subnet)
 #####################################################
+
+
+install_aws_cli() {
+
+	#########################################################
+	# BEGIN
+	#########################################################
+	log "BEGIN setup.sh"
+
+	unameOut="$(uname -s)"
+	case "${unameOut}" in
+	  Linux*)     machine=Linux;;
+	  Darwin*)    machine=Mac;;
+	  CYGWIN*)    machine=Cygwin;;
+	  MINGW*)     machine=MinGw;;
+	  *)          machine="UNKNOWN:${unameOut}"
+	esac
+	log "Current machine is: $machine"
+
+	#####################################################
+	# first check if JQ is installed
+	#####################################################
+	log "Installing jq"
+
+	jq_v=`jq --version 2>&1`
+	if [[ $jq_v = *"command not found"* ]]; then
+	  if [[$machine = "Mac" ]]; then
+	    curl -L -s -o jq "https://github.com/stedolan/jq/releases/download/jq-1.6/jq-osx-amd64"
+	  else
+	    curl -L -s -o jq "https://github.com/stedolan/jq/releases/download/jq-1.6/jq-linux64"
+	  fi 
+	  chmod +x ./jq
+	  cp jq /usr/bin
+	else
+	  log "jq already installed. Skipping"
+	fi
+
+	jq_v=`jq --version 2>&1`
+	if [[ $jq_v = *"command not found"* ]]; then
+	  log "error installing jq. Please see README and install manually"
+	  echo "Error installing jq. Please see README and install manually"
+	  exit 1 
+	fi  
+
+}
+
