640c0a162b135b92d244868f5fa69a9b7b86ae8b
root <rtlepple@gmail.com>
2019-08-26 00:28:23 +0000

    initial commit

--- bin/setup.sh
+++ bin/setup.sh
@@ -22,3 +22,49 @@ case "$1" in
 esac
 
 CLOUD_PROVIDER=$1
+
+#########################################################
+# utility functions
+#########################################################
+dir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+
+starting_dir=`pwd`
+
+# logging function
+log() {
+    echo -e "[$(date)] [$BASH_SOURCE: $BASH_LINENO] : $*"
+    echo -e "[$(date)] [$BASH_SOURCE: $BASH_LINENO] : $*" >> $starting_dir/setup-all.log
+}
+# Load util functions.
+#. $starting_dir/bin/utils.sh
+
+
+#########################################################
+# BEGIN
+#########################################################
+log "BEGIN setup.sh"
+
+#####################################################
+# first check if JQ is installed
+#####################################################
+log "Installing jq"
+
+jq_v=`jq --version 2>&1`
+if [[ $jq_v = *"command not found"* ]]; then
+  if [[ $machine = "Mac" ]]; then
+    sudo curl -L -s -o jq "https://github.com/stedolan/jq/releases/download/jq-1.6/jq-osx-amd64"
+  else
+    sudo curl -L -s -o jq "https://github.com/stedolan/jq/releases/download/jq-1.6/jq-linux64"
+  fi 
+  sudo chmod +x ./jq
+  sudo cp jq /usr/bin
+else
+  log "jq already installed. Skipping"
+fi
+
+jq_v=`jq --version 2>&1`
+if [[ $jq_v = *"command not found"* ]]; then
+  log "error installing jq. Please see README and install manually"
+  echo "Error installing jq. Please see README and install manually"
+  exit 1 
+fi  
