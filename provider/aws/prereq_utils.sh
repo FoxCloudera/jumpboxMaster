356c32b09bb15a45ed55e99df03582190a222eb1
root <rtlepple@gmail.com>
2019-08-26 19:08:32 +0000

    initial commit

--- provider/aws/prereq_utils.sh
+++ provider/aws/prereq_utils.sh
@@ -8,6 +8,106 @@
 # Functions: create AWS prereqs (VPC, subnets, security group, db subnet)
 #####################################################
 
+create_prereqs() {
+  if [ -s .info ]; then
+    log "Looks like you have not propertly terminated the previous environment, as there are still entries in .info. Execute first terminate.sh and rerun. Exiting..."
+    exit 1
+  fi
+
+  #####################################################
+  # record main info for the cluster
+  #####################################################
+  echo "AWS_REGION=${AWS_REGION:?}" > .info
+  echo "OWNER=${OWNER:?}" >> .info
+  echo "PROJECT='${PROJECT:?}'" >> .info
+  echo "CDH_VERSION=${CDH_VERSION:?}" >> .info
+
+  #####################################################
+  # create VPC
+  #####################################################
+  if [ "${MY_VPC}" = "" ]; then
+    vpc_id=`aws --output json --region ${AWS_REGION:?} ec2 create-vpc --cidr-block 10.0.0.0/16 | jq -r ".Vpc.VpcId"`
+    if [ "${vpc_id}" = "" ]; then
+      log "VPC could not be created. Most likely the limit has beeen exceeded. Please pick a different region, or use an existing VPC by setting 'export MY_VPC=...' and rerun the script. Exiting..."
+      exit 1
+    fi
+    echo "existingVpc=false" >> $starting_dir/provider/aws/.info
+    aws --region ${AWS_REGION:?} ec2 create-tags --resources ${vpc_id:?} --tags Key=owner,Value=${OWNER_TAG:?} Key=Name,Value=${OWNER_TAG:?}-ingest-demo
+    log "New VPC in ${AWS_REGION:?} created: ${OWNER_TAG:?}-ingest-demo, ${vpc_id:?}"
+  else
+    vpc_id="${MY_VPC}"
+    echo "existingVpc=true" >> $starting_dir/provider/aws/.info
+    log "Existing VPC in ${AWS_REGION:?} used: ${vpc_id:?}"
+  fi
+  echo "vpc_id=${vpc_id:?}" >> $starting_dir/provider/aws/.info
+  aws --region ${AWS_REGION:?} ec2 modify-vpc-attribute --enable-dns-hostnames --vpc-id ${vpc_id:?}
+ 
+  ##################################################### 
+  # create public subnets
+  #####################################################
+  subnet_id=`aws --output json --region ${AWS_REGION:?} ec2 create-subnet --availability-zone ${AWS_REGION:?}a --vpc-id ${vpc_id:?} --cidr-block 10.0.8.0/24 | jq -r ".Subnet.SubnetId"`
+  echo "subnet_id=${subnet_id:?}" >> $starting_dir/provider/aws/.info
+  log "New Subnet in ${AWS_REGION:?}a created: ${OWNER_TAG:?}-ingest-demo, ${subnet_id:?}"
+  aws --region ${AWS_REGION:?} ec2 create-tags --resources ${subnet_id:?} --tags Key=owner,Value=${OWNER_TAG:?} Key=Name,Value=${OWNER_TAG:?}-ingest-demo
+ 
+  ##################################################### 
+  # create/get internet gateway
+  #####################################################
+  igw=`aws --region ${AWS_REGION:?} ec2 describe-internet-gateways --filters Name=attachment.vpc-id,Values=${vpc_id:?} | jq -r ".InternetGateways[0].InternetGatewayId"`
+  if [ "$igw" == "null" ]; then
+    igw=`aws --output json --region ${AWS_REGION:?} ec2 create-internet-gateway | jq -r ".InternetGateway.InternetGatewayId"`
+    aws --region ${AWS_REGION:?} ec2 attach-internet-gateway --vpc-id ${vpc_id:?} --internet-gateway-id ${igw}
+  fi
+  echo "igw=${igw:?}" >> $starting_dir/provider/aws/.info
+  log "Internet gateway used: ${igw:?}"
+ 
+  ##################################################### 
+  # create route table
+  #####################################################
+  rtb=`aws --output json --region ${AWS_REGION:?} ec2 create-route-table --vpc-id ${vpc_id:?} | jq -r ".RouteTable.RouteTableId"`
+  aws --region ${AWS_REGION:?} ec2 create-route --route-table-id ${rtb:?} --destination-cidr-block 0.0.0.0/0 --gateway-id ${igw:?}
+  aws --region ${AWS_REGION:?} ec2 associate-route-table  --subnet-id ${subnet_id:?} --route-table-id ${rtb:?}
+  echo "rtb=${rtb:?}" >> $starting_dir/provider/aws/.info
+  log "Route table used: ${rtb:?}"
+ 
+  ##################################################### 
+  # get PEM file
+  #####################################################
+  aws --region ${AWS_REGION:?} ec2 create-key-pair --key-name ${OWNER_TAG:?}-ingest-demo --query 'KeyMaterial' --output text > ${OWNER_TAG:?}-ingest-demo.pem
+  chmod 400 ${OWNER_TAG:?}-ingest-demo.pem
+
+  #####################################################
+  # create Security Group
+  #####################################################
+  sg=`aws --output json --region ${AWS_REGION:?} ec2 create-security-group --group-name ${OWNER_TAG:?}-ingest-demo-SG --description "Security group for Ingest Demo" --vpc-id ${vpc_id:?} | jq -r ".GroupId"`
+  aws --region ${AWS_REGION:?} ec2 authorize-security-group-ingress --group-id ${sg:?} --protocol all --port 0-65535 --source-group ${sg:?}
+  #  need to add a port 22 access here...
+  #  this next one might need to be removed...  its sets the ip allowed to the public ip address of the host running this code.  (jumpbox).
+  # orig version 
+  aws --region ${AWS_REGION:?} ec2 authorize-security-group-ingress --group-id ${sg:?} --protocol all --port 0-65535 --cidr `curl -s ipinfo.io/ip`/32
+  # added this to map to my home ip address and not the jumpbox server
+  #aws --region ${AWS_REGION:?} ec2 authorize-security-group-ingress --group-id ${sg:?} --protocol all --port 0-65535 --cidr ${MY_HOME_IP:?}
+  echo "sg=${sg:?}" >> $starting_dir/provider/aws/.info
+  log "New Security Group in ${AWS_REGION:?} created: ${OWNER_TAG:?}-ingest-demo-SG, ${sg:?}"
+
+  
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+#####################################################
+# Function to install aws cli
+#####################################################
 
 install_aws_cli() {
 
@@ -53,3 +153,13 @@ install_aws_cli() {
 
 }
 
+#####################################################
+# Function to create director instance
+#####################################################
+create_onenode_instance() {
+	log "Create oneNode ec2 instance"
+	oneNodeInstanceId=`aws --output json --region ${AWS_REGION:?} ec2 run-instances --image-id ${AMI_ID:?} --key-name ${OWNER_TAG:?}-ingest-demo --security-group-ids ${sg:?} --instance-type ${ONE_NODE_INSTANCE:?} --subnet-id ${subnet_id:?} --associate-public-ip-address | jq -r ".Instances[0].InstanceId"`
+	log "Instance ID: ${oneNodeInstanceId:?}"
+	aws --region ${AWS_REGION:?} ec2 create-tags --resources ${oneNodeInstanceId:?} --tags Key=owner,Value=${OWNER_TAG:?} Key=Name,Value=ingest-director-${OWNER_TAG:?} Key=ingest-demo,Value=${OWNER_TAG:?} Key=project,Value=${PROJECT_TAG:?}
+	echo "oneNodeInstanceId=${oneNodeInstanceId:?}" >> ./.info
+}
