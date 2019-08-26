541517b05f82eba2794ba948fabc5aca9c2c729e
root <rtlepple@gmail.com>
2019-08-26 15:06:29 +0000

    initial commit

--- bin/setup.sh
+++ bin/setup.sh
@@ -39,48 +39,6 @@ log() {
 #. $starting_dir/bin/utils.sh
 
 
-#########################################################
-# BEGIN
-#########################################################
-log "BEGIN setup.sh"
-
-unameOut="$(uname -s)"
-case "${unameOut}" in
-  Linux*)     machine=Linux;;
-  Darwin*)    machine=Mac;;
-  CYGWIN*)    machine=Cygwin;;
-  MINGW*)     machine=MinGw;;
-  *)          machine="UNKNOWN:${unameOut}"
-esac
-log "Current machine is: $machine"
-
-#####################################################
-# first check if JQ is installed
-#####################################################
-log "Installing jq"
-
-jq_v=`jq --version 2>&1`
-if [[ $jq_v = *"command not found"* ]]; then
-  if [[ $machine = "Mac" ]]; then
-    curl -L -s -o jq "https://github.com/stedolan/jq/releases/download/jq-1.6/jq-osx-amd64"
-  else
-    curl -L -s -o jq "https://github.com/stedolan/jq/releases/download/jq-1.6/jq-linux64"
-  fi 
-  chmod +x ./jq
-  cp jq /usr/bin
-else
-  log "jq already installed. Skipping"
-fi
-
-jq_v=`jq --version 2>&1`
-if [[ $jq_v = *"command not found"* ]]; then
-  log "error installing jq. Please see README and install manually"
-  echo "Error installing jq. Please see README and install manually"
-  exit 1 
-fi  
-
-
-
 #####################################################
 #
 #####################################################
