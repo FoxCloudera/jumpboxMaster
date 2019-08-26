b2cb7e363dee4d057f7010a2ddef0258e64a5686
root <rtlepple@gmail.com>
2019-08-26 14:17:32 +0000

    initial commit

--- bin/setup.sh
+++ bin/setup.sh
@@ -88,21 +88,22 @@ fi
 case "$CLOUD_PROVIDER" in
         aws)
             echo "execute aws code here.  consider moving to a script.  load functions "
-	    log "Installing AWS_CLI"
-  	    aws_cli_version=`aws --version 2>&1`
-  	    log "Current CLI version: $aws_cli_version"
-  	    if [[ $aws_cli_version = *"aws-cli"* ]]; then
-    		log "AWS CLI already installed. Skipping"
-    		return
-  	    fi
-  	    if [ $machine = 'Linux' ]; then
-    		yum -y install unzip
-  	    fi 
-  	    curl -s -O "https://s3.amazonaws.com/aws-cli/awscli-bundle.zip"
-  	    unzip awscli-bundle.zip
-  	     ./awscli-bundle/install -i /usr/local/aws -b /usr/local/bin/aws
-	    rm -rf awscli-bundle*
-  	    log "Done installing AWS CLI"
+	    ./app/jumpboxMaster/provider/aws/aws_setup.sh
+#	    log "Installing AWS_CLI"
+#  	    aws_cli_version=`aws --version 2>&1`
+#  	    log "Current CLI version: $aws_cli_version"
+#  	    if [[ $aws_cli_version = *"aws-cli"* ]]; then
+#    		log "AWS CLI already installed. Skipping"
+#    		return
+#  	    fi
+#  	    if [ $machine = 'Linux' ]; then
+#    		yum -y install unzip
+#  	    fi 
+#  	    curl -s -O "https://s3.amazonaws.com/aws-cli/awscli-bundle.zip"
+#  	    unzip awscli-bundle.zip
+#  	     ./awscli-bundle/install -i /usr/local/aws -b /usr/local/bin/aws
+#	    rm -rf awscli-bundle*
+#  	    log "Done installing AWS CLI"
             ;;
         azure)
            echo "execute azure code here"
