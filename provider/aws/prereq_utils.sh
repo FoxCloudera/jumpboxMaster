b46bc8b37602ee2a87ff4c8cf7632441ad6f0bd7
root <rtlepple@gmail.com>
2019-08-28 21:09:12 +0000

    initial commit

--- provider/aws/prereq_utils.sh
+++ provider/aws/prereq_utils.sh
@@ -201,6 +201,25 @@ install_aws_cli() {
 	  exit 1 
 	fi  
 
+	####################################################
+ 	# then install AWS CLI
+	#####################################################
+  	log "Installing AWS_CLI"
+  	aws_cli_version=`aws --version 2>&1`
+  	log "Current CLI version: $aws_cli_version"
+  	if [[ $aws_cli_version = *"aws-cli"* ]]; then
+    		log "AWS CLI already installed. Skipping"
+    		return
+  	fi
+  	if [ $machine = 'Linux' ]; then
+    		sudo yum -y install unzip
+  	fi  
+  		sudo curl -s -O "https://s3.amazonaws.com/aws-cli/awscli-bundle.zip"
+  		sudo unzip awscli-bundle.zip
+  		sudo ./awscli-bundle/install -i /usr/local/aws -b /usr/local/bin/aws 
+  		sudo rm -rf awscli-bundle*
+  	log "Done installing AWS CLI"
+
 }
 
 #####################################################
